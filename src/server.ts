dc0cbfed38229400ed8d5e5c9ca0d25b7b0c0a2a
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2025-03-29 03:44:31 +0600

    initialize project

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import app from './app';
 import config from './app/config/config';
+import mongoose from 'mongoose';
 
 async function main() {
   try {
-    // await myDatabase.connect (config.database_url as string)
-    app.listen(config.port, () => {
+    console.log("hello from server calling!");
+    console.log("Database URL:", config.database_url); // Debugging
+    await mongoose.connect(config.database_url as string);
+    console.log("hello after server calling!");
+
+    app.listen(config.port, async () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
